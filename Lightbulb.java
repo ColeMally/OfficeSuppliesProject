79bb6e27f25441f30b65473ecb67dea48f28bab5
Junaid Bhatti <jbhatti24@students.polytechnic.org>
2023-01-31 08:57:50 -0800

    done

--- Lightbulb.java
+++ Lightbulb.java
@@ -1,3 +1,34 @@
 public class Lightbulb {
-    
+    //fields
+    private int light;
+
+    //constructors
+    public Lightbulb(int myLight){
+        light = myLight;
+    }
+
+    //methods
+    public int turnOn(){
+        light = 100;
+        return light;
+    }
+    public int turnOff(){
+        light = 0;
+        return light;
+    }
+    public int Brighten(){
+        light += 50;
+        if(light >= 100){
+            light = 100;
+        }
+        return light;
+    }
+    public int Dim(){
+        light -= 50;
+        if(light <= 0){
+            light = 0;
+        }
+        return 0;
+    }
+
 }
