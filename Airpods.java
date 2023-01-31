6d5d743fe0cabd947239f15be6320b72c89d1fb6
Junaid Bhatti <jbhatti24@students.polytechnic.org>
2023-01-31 09:09:17 -0800

    airpods

--- Airpods.java
+++ Airpods.java
@@ -1,5 +1,49 @@
 public class Airpods {
+    //fields
     private int durability;
+    private int battery;
+    private boolean connected;
+    private boolean charging;
+
+    //constructors
+    public Airpods(int myDurability, int myBattery, boolean myConnected, boolean myCharging){
+        durability = myDurability;
+        battery = myBattery;
+        connected = myConnected;
+        charging = myCharging;
+    }
 
 
+    //methods
+    public boolean Connect(){
+        connected = true;
+        return connected;
+    }
+    public boolean Disconnect(){
+        connected = false;
+        return connected;
+    }
+    public boolean Charge(){
+        charging = true;
+        return charging;
+    }
+    public boolean notCharging(){
+        charging = false;
+        return charging;
+    }
+
+    public boolean checkCharging(){
+        if(charging == true){
+            battery += 5;
+        }else{
+            System.out.println("The case is not charging");
+        }
+        System.out.println(battery);
+        return charging;
+
+    }
+    public int useAirpods(){
+        battery -= 10;
+        return battery;
+    }
 }
